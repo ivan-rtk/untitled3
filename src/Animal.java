34fc0ec7cdfdb14f2829a74b030788cf514487ca
Иван <78495492+ivan-rtk@users.noreply.github.com>
2024-09-01 14:22:05 +0300

    Update Animal.java

--- src/Animal.java
+++ src/Animal.java
@@ -1,13 +1,1 @@
-public class Animal {
-    static int animalCount = 0;
 
-    public Animal() {
-        animalCount++;
-    }
-
-    public void run(int distance) {
-    }
-
-    public void swim(int distance) {
-    }
-}
